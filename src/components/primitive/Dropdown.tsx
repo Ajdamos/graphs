0c485cbccb981c481801c8764be61bd2ddec543d
Ajdamos <100752422+Ajdamos@users.noreply.github.com>
2024-02-19 02:17:10 +0100

    Update Dropdown.tsx

--- src/components/primitive/Dropdown.tsx
+++ src/components/primitive/Dropdown.tsx
@@ -10,7 +10,8 @@ const DropMenu = (props: DropMenuProps) => {
     const defaultOption = props.label
     return (
         <Dropdown
-            className='flex flex-col justify-center items-center bg-lightTeal text-darksm font-bold text-l border-2 border-darksm w-[116px] h-10 p-4 rounded-lg'
+            className='flex flex-col items-center text-center bg-lightTeal text-darksm font-bold text-l border-2 border-darksm w-[116px] h-10 p-2 rounded-lg'
+            menuClassName='z-10 flex flex-col border-2 m-2 border-darksm bg-lightTeal text-darksm font-bold text-l w-[116px] p-2 rounded-lg'
             options={props.options}
             value={defaultOption.toString()}
             placeholder={props.label}    
