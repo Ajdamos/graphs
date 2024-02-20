dbfb971d6ab6e732b0d8f3602d1c139047c27f42
Ajdamos <100752422+Ajdamos@users.noreply.github.com>
2024-02-20 14:52:47 +0100

    Update Landing.tsx

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,11 +1,16 @@
-
+import React from "react"
+import Button from "../components/primitive/Button"
+import { ButtonDarkSM } from "../stories/Button.stories"
 
 export const Landing = () => {
 
     return (
-        <div>
-            <h1> Welcome to the Maze</h1>
-            <p> This is a maze game where you can play and solve mazes</p>
+        <div className="w-full h-full flex flex-col justify-center items-center">
+            <h1 className="text-4xl text-darksm font-bold h-16 mt-36 text-center">Path Solving by Adam Černoš</h1>
+            <p className="p-4 m-4 text-darksm text-3xl w-[60%] text-center">
+                V tomto projektu se podíváme na různé algoritmy pro hledání cest v grafu či bludišti, byl použit například A* nebo Breadth-first search.
+            </p>
+            <Button {...ButtonDarkSM.args} label="Github"/>
         </div>
     )
 }
